1be1b8961d5a56ec40fc15c662f03d3378b672b1
Kenneth Jensen <42805394+poochy442@users.noreply.github.com>
2022-05-30 00:07:21 +0200

    Combine

--- backend/Migrations/20220529200813_Initial.Designer.cs
+++ backend/Migrations/20220529200813_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace backend.Migrations
 {
     [DbContext(typeof(DBContext))]
-    [Migration("20220528214704_Initial")]
+    [Migration("20220529200813_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
